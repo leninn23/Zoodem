a3cb91ee760ade4a5bd1a0431deaae9303361275
AhkrinSan <squesadac01@gmail.com>
2024-12-03 13:34:07 +0100

    Tamaño máximo de la bola arreglo desapariición

---
+++ ZoodemSimulation/Assets/Scripts/Nido.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Animals;
+using UnityEngine;
+
+public class Nido : MonoBehaviour
+{
+    public int offspringCount;
+    public float timeLeftForSpawn;
+
+    public ABasicAnimal owner;
+    // public IAnimal owner;
+
+
+}
